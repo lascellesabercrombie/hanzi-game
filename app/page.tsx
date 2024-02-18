bf583988a41302133a0c5db86c26506f8b2a7bbe
Alex Grafen <alex.grafen@gmail.com>
2024-02-18 15:46:27 +0000

    feat: quiz for selected character

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,17 @@ const characterArray = ['的', '一', '是', '不', '	了']
 
 export default function Home() {
   const [showCard, setShowCard] = useState(false)
+  const [chosenCharacter, setChosenCharacter] = useState('')
+  const [characterMetadata, setCharacterMetadata] = useState(null)
   const targetDivRef = useRef(null);
   useEffect(() => {
     if (showCard) {
-      const writer = HanziWriter.create(targetDivRef.current, '我', {
+      const targetDiv = targetDivRef.current
+      if (targetDiv) {
+        targetDiv.replaceChildren('')
+      }
+
+      const writer = HanziWriter.create(targetDiv, chosenCharacter, {
         width: 100,
         height: 100,
         padding: 5,
@@ -22,16 +29,17 @@ export default function Home() {
         writer.hideCharacter();
       };
     }
-  }, [showCard]);
+  }, [chosenCharacter, showCard]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={() => { setShowCard(true) }}>Show card</button>
       {showCard &&
         <div className="py-20 px-20 min-h-7 max-w-sm mx-auto bg-red-200 rounded-xl shadow-lg space-y-5 ">
           <div ref={targetDivRef}></div>
         </div>}
-
+      {characterArray.map((character) =>
+        <button key={`button-${character}`} onClick={() => { setShowCard(true); setChosenCharacter(character) }}>{character}</button>
+      )}
     </main>
   );
 }
