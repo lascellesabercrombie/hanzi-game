b37db99f7669d4b2cf78d10dede221dc89b8efef
Alex Grafen <alex.grafen@gmail.com>
2024-02-28 17:06:18 +0000

    refactor: extract validator into separate file

---
+++ src/helpers/validateInput.ts
@@ -0,0 +1,11 @@
+export const validateInput = (input: string) => {
+    const chineseCharacterRegex = /^[\u4e00-\u9fa5]{0,1}$/
+    const englishWordRegex = /^[a-zA-Z]+$/
+
+    if (chineseCharacterRegex.test(input)) {
+        return "chinese"
+    }
+    if (englishWordRegex.test(input)) {
+        return "english"
+    }
+}
