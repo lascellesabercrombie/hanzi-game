7824a4ce679231050ca4d0a90fb2167e6fe85e29
Alex Grafen <alex.grafen@gmail.com>
2024-03-11 12:51:27 +0000

    refactor: abstraction for icon button

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,19 @@
+interface IconButtonProps {
+    ariaLabel: string,
+    disabled?: boolean,
+    size?: number,
+    onClick: () => void,
+    children: React.ReactElement
+}
+
+export const IconButton = ({ ariaLabel, disabled = false, size = 8, onClick, children }: IconButtonProps) => {
+    const widthAndHeight = `w-${size} h-${size}`
+    return (
+        <button aria-disabled={disabled} disabled={disabled} className={
+            `bg-neutral-200 flex justify-center items-center ${widthAndHeight} rounded-full shadow-lg disabled:border disabled:bg-red-200 disabled:border-cyan-950 disabled:shadow-none`
+        }
+            aria-label={ariaLabel} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
