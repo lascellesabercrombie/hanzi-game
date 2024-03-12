a8c0f3f957aba93deaa80b580269b77392b749aa
Alex Grafen <alex.grafen@gmail.com>
2024-03-12 18:17:38 +0000

    feat: restyle settings to use switch

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { getInitialState } from "@/src/helpers/getInitialState"
 import { onToggleSetting } from "@/src/helpers/onToggleSetting"
 import { Title } from "@/src/components/Title"
+import { Switch } from "@headlessui/react"
 
 export default function Settings() {
     const [isDefinitionVisible, setIsDefinitionVisible] = useState(getInitialState('isDefinitionVisible', true))
@@ -33,24 +34,37 @@ export default function Settings() {
     return (
         <main>
             <Title>Settings</Title>
-            <ul className="toppings-list">
-                {options.map(({ name, localStorageKey, state, setState }, index) => {
-                    return (
-                        <li key={index}>
-                            <div className="left-section">
-                                <input
-                                    type="checkbox"
-                                    id={`checkbox-${name}`}
-                                    name={name}
-                                    value={name}
-                                    checked={state}
-                                    onChange={() => onToggleSetting(localStorageKey, state, setState)}
-                                />
-                                <label htmlFor={`custom-checkbox-${name}`}>{name}</label>
-                            </div>
-                        </li>
-                    );
-                })}
-            </ul>
+            <div className="p-4">
+                <ul className="py-4 px-6 border-2 border-slate-200 rounded-md">
+                    {options.map(({ name, localStorageKey, state, setState }, index) => {
+                        return (
+                            <li key={index}>
+                                <div>
+                                    <Switch.Group>
+                                        <div className="flex items-center py-2">
+                                            <Switch
+                                                name={name}
+                                                value={name}
+                                                checked={state}
+                                                className={`${state ? 'bg-cyan-950' : 'bg-cyan-700'}
+                                            relative inline-flex h-5 w-10 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
+                                                id={`checkbox-${name}`}
+                                                onChange={() => onToggleSetting(localStorageKey, state, setState)}
+                                            >
+                                                <span
+                                                    aria-hidden="true"
+                                                    className={`${state ? 'translate-x-5' : 'translate-x-0'}
+                                                 pointer-events-none inline-block h-4 w-4 transform rounded-full bg-slate-100 shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                                />
+                                            </Switch>
+                                            <Switch.Label className="pl-2">{name}</Switch.Label>
+                                        </div>
+                                    </Switch.Group>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
         </main>)
 }
