4403ba55cdf0a1313966a1aaafb3f81e4ddcc4e8
Alex Grafen <alex.grafen@gmail.com>
2024-03-12 14:21:54 +0000

    feat: move search, add character into modal

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,29 +1,35 @@
 'use client'
-import { validateInput } from "@/src/helpers/validateInput";
-import { convertPinyin } from "@/src/helpers/ccdbUtils";
-import { Suspense, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CharacterContext, CharacterContextType } from "../../components/ParentWrapper";
+import { SearchModal } from "@/src/components/SearchModal";
 import { useRouter } from 'next/navigation'
 import { Title } from "@/src/components/Title";
+import SvgAdd from "@/public/character/SvgAdd";
 
 
-type SearchResult = {
-    character: string,
-    definition?: string,
-    pronunciation?: string,
-}
 
-type FetchDefinitionResultItem = {
-    string: string,
-    kDefinition?: string,
-    kMandarin?: string
-}
 
 export default function Library() {
     const [characterSet, setCharacterSet] = useState(new Set<string>())
-    const [searchResults, setSearchResults] = useState<null | Array<SearchResult>>(null)
+    const [isModalOpen, setIsModalOpen] = useState(false)
     const { onSelectChosenCharacter } = useContext(CharacterContext) as CharacterContextType
     const router = useRouter()
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const onAddToCharacterSet = (itemToAdd: string) => {
+        setCharacterSet((characterSet) => {
+            const newSet = new Set(characterSet);
+            newSet.add(itemToAdd);
+            return newSet;
+        });
+    }
 
     useEffect(() => {
         const storedCharacters = JSON.parse(localStorage.getItem('characters') || '{}', (key, value) => {
@@ -50,61 +56,17 @@ export default function Library() {
             <div className="flex flex-col pb-4">
                 <Title>Library</Title>
             </div>
-            <form action={(formData) => {
-                const query = formData.get("newCharacter");
-                if (typeof query === "string" && validateInput(query) === "chinese") {
-                    setCharacterSet((characterSet) => {
-                        const newSet = new Set(characterSet);
-                        newSet.add(query);
-                        return newSet;
-                    });
-                }
-                if (typeof query === "string" && validateInput(query) === "english") {
-                    fetch(`http://ccdb.hemiola.com/characters/definition/${query}?fields=string,kDefinition,kMandarin`)
-                        .then((response) => { setSearchResults([]); return response.json(); })
-                        .then((data) => {
-                            const array: Array<SearchResult> = []
-                            if (data) {
-                                data.map((datum: FetchDefinitionResultItem) => {
-                                    array.push({ character: datum["string"], definition: datum["kDefinition"], pronunciation: convertPinyin(datum?.["kMandarin"]?.split(" ")?.[0]) })
-                                })
-                                setSearchResults(array)
-                            }
-                        })
-                }
-            }}>
-                <label htmlFor="characterInput">Add a character to your library</label>
-                <input id="characterInput" type="text" name="newCharacter" pattern="^[a-zA-Z]+$|^[\u4E00-\u9FFF]{1}$"></input>
-                <button type="submit">submit</button>
-            </form>
-            <Suspense fallback={<p>Loading</p>}>
-                <div className="flex flex-col gap-2 py-2 px-4">
-                    {(searchResults && searchResults.length === 0) && (<div>No results found</div>)}
-                    {searchResults && searchResults.map((searchItem, index) => {
-                        return (
-                            <button className="bg-slate-200 text-cyan-950 flex gap-4 rounded-xl shadow-lg px-4 py-2 items-center" key={`search-result-${index}`} onClick={() => {
-                                setCharacterSet((characterSet) => {
-                                    const newSet = new Set(characterSet);
-                                    newSet.add(searchItem["character"]);
-                                    return newSet;
-                                })
-                                setSearchResults([])
-                            }}>
-                                <h2 className="text-3xl">{searchItem["character"]}</h2>
-                                <div className="flex flex-col text-left">
-                                    <h3 className="text-lg">{searchItem["pronunciation"]}</h3>
-                                    <h4 className="text-base">{searchItem["definition"]}</h4>
-                                </div>
-                            </button>
-                        )
-                    })
-                    }
-                </div>
-            </Suspense>
-            <h3>Press a character to practise writing it</h3>
-            <div className="grid grid-cols-3 gap-6 px-6 py-2 ">
+            <div className="flex flex-col justify-center px-6 py-2 gap-3">
+                <button className="bg-cyan-700 flex rounded-lg pl-2 pr-4 py-2 w-fit shadow-lg text-slate-200 font-medium justify-center items-center" onClick={openModal}>
+                    <SvgAdd className="w-8 h-8 *:stroke-slate-200" />
+                    <span>Add character to library</span>
+                </button>
+                <h3>Press a character to practise writing it</h3>
+            </div>
+
+            <div className="grid grid-cols-3 gap-6 px-6 py-2">
                 {Array.from(characterSet).map((character) =>
-                    <button className="py-6 px-2 bg-blue-100 rounded-xl text-3xl text-cyan-950 shadow-lg" key={`button-${character}`}
+                    <button className="py-6 px-2 max-w-28 bg-blue-100 rounded-xl text-3xl text-cyan-950 shadow-lg" key={`button-${character}`}
                         onClick={() => {
                             onSelectChosenCharacter(character);
                             router.push('/practice')
@@ -112,5 +74,7 @@ export default function Library() {
                     >{character}</button>
                 )}
             </div>
-        </section>)
+            <SearchModal isModalOpen={isModalOpen} closeModal={closeModal} onAddToCharacterSet={onAddToCharacterSet} />
+        </section>
+    )
 }
---
+++ src/components/SearchModal.tsx
@@ -0,0 +1,116 @@
+import { Fragment, Suspense, useContext, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react'
+import { CharacterContext, CharacterContextType } from './ParentWrapper'
+import { validateInput } from '../helpers/validateInput';
+import { convertPinyin } from '../helpers/ccdbUtils';
+
+interface SearchModalProps {
+    isModalOpen: boolean,
+    closeModal: () => void,
+    onAddToCharacterSet: (itemToAdd: string) => void
+}
+
+type SearchResult = {
+    character: string,
+    definition?: string,
+    pronunciation?: string,
+}
+
+type FetchDefinitionResultItem = {
+    string: string,
+    kDefinition?: string,
+    kMandarin?: string
+}
+
+export const SearchModal = ({ isModalOpen, closeModal, onAddToCharacterSet }: SearchModalProps) => {
+    const { chosenCharacter } = useContext(CharacterContext) as CharacterContextType
+    const [searchResults, setSearchResults] = useState<null | Array<SearchResult>>(null)
+
+    return (<Transition appear show={isModalOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="fixed inset-0 bg-black/25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                    >
+                        <Dialog.Panel className="w-full max-w-sm transform overflow-hidden rounded-2xl bg-neutral-200 p-10 text-left align-middle shadow-xl transition-all">
+                            <Dialog.Title
+                                as="h3"
+                                className="text-lg font-medium leading-6"
+                            >
+                                Add character to library
+                            </Dialog.Title>
+                            <div className="flex flex-col mt-6 justify-between">
+                                <form action={(formData) => {
+                                    const query = formData.get("newCharacter");
+                                    if (typeof query === "string" && validateInput(query) === "chinese") {
+                                        onAddToCharacterSet(query)
+                                        closeModal()
+                                    }
+                                    if (typeof query === "string" && validateInput(query) === "english") {
+                                        fetch(`http://ccdb.hemiola.com/characters/definition/${query}?fields=string,kDefinition,kMandarin`)
+                                            .then((response) => { setSearchResults([]); return response.json(); })
+                                            .then((data) => {
+                                                const array: Array<SearchResult> = []
+                                                if (data) {
+                                                    data.map((datum: FetchDefinitionResultItem) => {
+                                                        array.push({ character: datum["string"], definition: datum["kDefinition"], pronunciation: convertPinyin(datum?.["kMandarin"]?.split(" ")?.[0]) })
+                                                    })
+                                                    setSearchResults(array)
+                                                }
+                                            })
+                                    }
+                                }}>
+                                    <label htmlFor="characterInput">Search by character or English definition</label>
+                                    <div className="flex">
+                                        <input id="characterInput" type="text" name="newCharacter" pattern="^[a-zA-Z]+$|^[\u4E00-\u9FFF]{1}$"></input>
+                                        <button type="submit">submit</button>
+                                    </div>
+                                </form>
+                                <Suspense fallback={<p>Loading</p>}>
+                                    <div className="flex flex-col gap-2 py-2 px-4">
+                                        {(searchResults && searchResults.length === 0) && (<div>No results found</div>)}
+                                        {searchResults && searchResults.map((searchItem, index) => {
+                                            return (
+                                                <button className="bg-slate-200 text-cyan-950 flex gap-4 rounded-xl shadow-lg px-4 py-2 items-center" key={`search-result-${index}`} onClick={() => {
+                                                    onAddToCharacterSet(searchItem["character"])
+                                                    setSearchResults([])
+                                                    closeModal()
+                                                }}>
+                                                    <h2 className="text-3xl">{searchItem["character"]}</h2>
+                                                    <div className="flex flex-col text-left">
+                                                        <h3 className="text-lg">{searchItem["pronunciation"]}</h3>
+                                                        <h4 className="text-base">{searchItem["definition"]}</h4>
+                                                    </div>
+                                                </button>
+                                            )
+                                        })
+                                        }
+                                    </div>
+                                </Suspense>
+                            </div>
+                        </Dialog.Panel>
+                    </Transition.Child>
+                </div>
+            </div>
+        </Dialog>
+    </Transition>)
+}
