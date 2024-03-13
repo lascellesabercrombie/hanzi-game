3a4c8e0a74f2f4915578d4e9583cc02e25dd45d9
Alex Grafen <alex.grafen@gmail.com>
2024-03-13 17:34:36 +0000

    feat: add search svg component

---
+++ public/character/SvgSearch.tsx
@@ -0,0 +1,19 @@
+const SvgSearch = (props: { className: string }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={800}
+        height={800}
+        fill="none"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            stroke="#000"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            d="M14.954 14.946 21 21m-4-11a7 7 0 1 1-14 0 7 7 0 0 1 14 0"
+        />
+    </svg>
+);
+export default SvgSearch;
