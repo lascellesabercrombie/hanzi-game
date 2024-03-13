454b0b4122a48cbde72a3232fa7828a428c45887
Alex Grafen <alex.grafen@gmail.com>
2024-03-13 19:03:30 +0000

    fix: install slick-carousel for css, move import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import "../../node_modules/slick-carousel/slick/slick.css";
+import "../../node_modules/slick-carousel/slick/slick-theme.css";
+
 import Navbar from "../components/Navbar"
 import ParentWrapper from "../components/ParentWrapper";
 import './globals.css'
