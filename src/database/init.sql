b28307a3a4489b1f4c0ea72eee48a114685b18ca
Alex Grafen <alex.grafen@gmail.com>
2024-03-18 22:23:58 +0000

    fix: ensure immutability

--- src/database/init.sql
+++ src/database/init.sql
@@ -10,7 +10,12 @@ CREATE TABLE unihan_characters (
     kmandarin TEXT,
     khanyupinyin TEXT
     );
+CREATE FUNCTION immutable_tsvector(text) RETURNS tsvector AS $$
+BEGIN
+    RETURN to_tsvector('simple', $1);
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
 
-CREATE INDEX name ON unihan_characters USING GIN (to_tsvector(kdefinition));
+CREATE INDEX unihan_character_index ON unihan_characters USING gin(immutable_tsvector(kdefinition));
 
 COMMIT;
