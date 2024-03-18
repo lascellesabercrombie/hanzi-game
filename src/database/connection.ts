1bc9ebb7cdf8d13b0f425e3d3269aba70deb3418
Alex Grafen <alex.grafen@gmail.com>
2024-03-18 12:34:14 +0000

    feat: connect database

---
+++ src/database/connection.ts
@@ -0,0 +1,9 @@
+const pg = require("pg");
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+export const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
