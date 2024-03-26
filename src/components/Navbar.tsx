c994cf5d64df709fd29accb14470eda14dd996fb
Alex Grafen <alex.grafen@gmail.com>
2024-03-26 09:30:57 +0000

    fix: use border rather than outline

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,11 +12,11 @@ type NavBarItemType = [string, string, ReactElement<any, any>]
 export default function Navbar() {
 
   const pathname = usePathname()
-  const currentPageStyling = "outline outline-offset-2 outline-2 outline-blue-200"
+  const currentPageStyling = "border-solid border-2 border-blue-200"
   const navbarItems: Array<NavBarItemType> = [
-    ["Library", "/library", <SvgLibrary className={`${pathname === "/library" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/library" ? currentPageStyling : ''}`} key="library" />],
-    ["Practice", "/practice", <SvgPractice className={`${pathname === "/practice" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/practice" ? currentPageStyling : ''}`} key="practice" />],
-    ["Settings", "/settings", <SvgSettings className={`${pathname === "/settings" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/settings" ? currentPageStyling : ''}`} key="settings" />]
+    ["Library", "/library", <SvgLibrary className={`${pathname === "/library" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/library" ? currentPageStyling : 'border-solid border-2 border-transparent'}`} key="library" />],
+    ["Practice", "/practice", <SvgPractice className={`${pathname === "/practice" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/practice" ? currentPageStyling : 'border-solid border-2 border-transparent'}`} key="practice" />],
+    ["Settings", "/settings", <SvgSettings className={`${pathname === "/settings" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/settings" ? currentPageStyling : 'border-solid border-2 border-transparent'}`} key="settings" />]
   ]
 
   return (<ul className="bg-cyan-950 fixed border-t-2 border-slate-200 bottom-0 max-h-14 min-w-full flex justify-around">
