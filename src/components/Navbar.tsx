435981094864869540b2984f3cc62b53bfb1f20e
Alex Grafen <alex.grafen@gmail.com>
2024-03-26 09:35:57 +0000

    fix: padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,17 +12,18 @@ type NavBarItemType = [string, string, ReactElement<any, any>]
 export default function Navbar() {
 
   const pathname = usePathname()
-  const currentPageStyling = "border-solid border-2 border-blue-200"
+  const currentPageStyling = "fill-blue-200 border-blue-200"
+  const otherPageStyling = "fill-slate-200 border-transparent"
   const navbarItems: Array<NavBarItemType> = [
-    ["Library", "/library", <SvgLibrary className={`${pathname === "/library" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/library" ? currentPageStyling : 'border-solid border-2 border-transparent'}`} key="library" />],
-    ["Practice", "/practice", <SvgPractice className={`${pathname === "/practice" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/practice" ? currentPageStyling : 'border-solid border-2 border-transparent'}`} key="practice" />],
-    ["Settings", "/settings", <SvgSettings className={`${pathname === "/settings" ? "fill-blue-200" : "fill-slate-200"} rounded-lg max-h-full max-w-10 ${pathname === "/settings" ? currentPageStyling : 'border-solid border-2 border-transparent'}`} key="settings" />]
+    ["Library", "/library", <SvgLibrary className={`border-solid border-2 rounded-lg max-h-full max-w-10 p-0.5 ${pathname === "/library" ? currentPageStyling : otherPageStyling}`} key="library" />],
+    ["Practice", "/practice", <SvgPractice className={`border-solid border-2 rounded-lg max-h-full max-w-10 p-0.5 ${pathname === "/practice" ? currentPageStyling : otherPageStyling}`} key="practice" />],
+    ["Settings", "/settings", <SvgSettings className={`border-solid border-2 rounded-lg max-h-full max-w-10 p-0.5 ${pathname === "/settings" ? currentPageStyling : otherPageStyling}`} key="settings" />]
   ]
 
   return (<ul className="bg-cyan-950 fixed border-t-2 border-slate-200 bottom-0 max-h-14 min-w-full flex justify-around">
     {navbarItems.map(([name, link, ImageComponent]) => (
       <li className="flex" key={`navbar-item-${name}`}>
-        <Link href={link} className="p-2.5">
+        <Link href={link} className="p-2">
           {ImageComponent}
           <span className='sr-only'>{name}</span>
         </Link>
