3aa95f73d41f4e794bdff9e37e2a866acb80c41d
Alex Grafen <alex.grafen@gmail.com>
2024-05-03 17:08:47 +0200

    fix: only access property after checking window

--- src/helpers/getInitialStateNumber.ts
+++ src/helpers/getInitialStateNumber.ts
@@ -1,6 +1,9 @@
 export function getInitialStateNumber(key: string, fallback: number) {
+    if (typeof window === "undefined") {
+        return fallback
+    }
     const property = localStorage?.getItem(key)
-    if (typeof window === "undefined" || property === null || typeof Number.parseInt(property) !== 'number') {
+    if (property === null || typeof Number.parseInt(property) !== 'number') {
         return fallback
     }
     return Number.parseInt(property)
