3ee32ead249c1248529a77d685152845a1b10d16
Alex Grafen <alex.grafen@gmail.com>
2024-05-13 12:28:48 +0100

    feat: update availableSizes to include loading styles

--- src/app/constants/availableSizes.ts
+++ src/app/constants/availableSizes.ts
@@ -2,11 +2,12 @@ interface SizeOption {
     id: number,
     name: string,
     radioStyle: string,
+    skeletonStyle: string[],
     value: number
 }
 
 export const availableSizes: ReadonlyArray<SizeOption> = [
-    { id: 1, name: "Small", radioStyle: "text-sm", value: 100 },
-    { id: 2, name: "Medium", radioStyle: "text-lg", value: 200 },
-    { id: 3, name: "Large", radioStyle: "text-2xl", value: 400 }
+    { id: 1, name: "Small", radioStyle: "text-sm", skeletonStyle: ["h-[50px] w-[50px]", "h-[100px] w-[100px]"], value: 100 },
+    { id: 2, name: "Medium", radioStyle: "text-lg", skeletonStyle: ["h-[100px] w-[100px]", "h-[200px] w-[200px]"], value: 200 },
+    { id: 3, name: "Large", radioStyle: "text-2xl", skeletonStyle: ["h-[100px] w-[100px]", "h-[400px] w-[400px]"], value: 400 }
 ]
