a628b196b762d1e972588eee58e05c1239c6c9d7
Alex Grafen <alex.grafen@gmail.com>
2024-05-31 15:18:46 +0100

    fix: consistent size on select

--- src/components/CharacterGrid.tsx
+++ src/components/CharacterGrid.tsx
@@ -15,7 +15,7 @@ export const CharacterGrid = ({ isSelecting = false, selectedCharacterSet, selec
     return (<div className="p-6">
         <div className="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 justify-items-center min-w-[80vw] px-6 py-4 border-2 border-slate-200 rounded-xl" data-testid="div-character-grid">
             {Array.from(characterSet).map((character) =>
-                <button className={`py-2 px-2 min-w-[min(20vw,10rem)] min-h-[min(20vw,10rem)] bg-blue-100 rounded-xl text-[min(5rem,10vw)] text-cyan-950 drop-shadow-md hover:drop-shadow-xl active:drop-shadow-sm ${selectedCharacterSet && selectedCharacterSet.has(character) ? selectedStyling : ""}`} key={`button-${character}`}
+                <button className={`py-2 px-2 min-w-[min(20vw,10rem)] min-h-[min(20vw,10rem)] bg-blue-100 rounded-xl text-[min(5rem,10vw)] text-cyan-950 drop-shadow-md hover:drop-shadow-xl active:drop-shadow-sm ${selectedCharacterSet && selectedCharacterSet.has(character) ? selectedStyling : "border-4 border-transparent"}`} key={`button-${character}`}
                     onClick={isSelecting && selectCharacters ? () => {
                         selectCharacters(character)
                     } : () => {
