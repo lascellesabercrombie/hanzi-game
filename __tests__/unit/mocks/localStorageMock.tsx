4658492f749572ae250e461fe947395f1f800ec6
Alex Grafen <alex.grafen@gmail.com>
2024-06-10 11:21:13 +0100

    test: add local storage mock

---
+++ __tests__/unit/mocks/localStorageMock.tsx
@@ -0,0 +1,16 @@
+export const localStorageMock = (() => {
+  let store: { [key: string]: string } = {};
+
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => {
+      store[key] = value;
+    },
+    clear: () => {
+      store = {};
+    },
+    removeItem: (key: string) => {
+      delete store[key];
+    },
+  };
+})();
